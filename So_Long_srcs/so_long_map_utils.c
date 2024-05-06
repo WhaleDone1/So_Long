ad36dbdad6e83c623f7e132d0a4ec9740f8de69d
Brandon <brandonscott.carpenter@gmail.com>
2024-05-06 10:15:06 +0200

    Norm applied

--- So_Long_srcs/so_long_map_utils.c
+++ So_Long_srcs/so_long_map_utils.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long_map_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcarpent <bcarpent@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 10:03:48 by bcarpent          #+#    #+#             */
+/*   Updated: 2024/05/06 10:04:53 by bcarpent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
-int get_map(t_data *data, int collumn_count, int line_count)
+int	get_map(t_data *data, int collumn_count, int line_count)
 {
-if ((check_map_borders(data->map, collumn_count, line_count)) != 1
+	if ((check_map_borders(data->map, collumn_count, line_count)) != 1
 		|| (check_map_requirements(data->map, 0, 0, 0, data) != 1)
 		|| (so_long_solver(data) != 1))
 		return (-1);
-return (0);
+	return (0);
 }
